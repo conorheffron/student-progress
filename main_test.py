0e3ab1b5fcbe6ee4248ecb7e5b733d199170a260
Conor Heffron <conor.heffron@gmail.com>
2025-10-16 22:57:18 +0100

    Add tests for small data approaches (#9)
    * Add tests for small data approaches
    
    * Update assertions in test cases for book details
    
    * Revise comments for clarity on book reading program
    
    Updated the introductory comments to clarify the file source and the output format.

--- main_test.py
+++ main_test.py
@@ -12,3 +12,15 @@ def test_raw_csv_requests_approach():
         "https://conorheffron.github.io/student-books-test-data/students/books/data.txt")
 
     assert result == (20015, 2805094)
+
+def test_pandas_sm_approach():
+    result = get_book_details_pandas(
+        "https://conorheffron.github.io/student-books-test-data/students/books/test_sm_data.txt")
+
+    assert result == (4, 850)
+
+def test_raw_csv_sm_requests_approach():
+    result = get_book_details_requests_csv(
+        "https://conorheffron.github.io/student-books-test-data/students/books/test_sm_data.txt")
+
+    assert result == (4, 850)
